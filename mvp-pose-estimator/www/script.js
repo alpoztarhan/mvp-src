5c87c9c5ba6e6aed84aee3e1885f0b0c0df3c028
barancankatesen <baran@DESKTOP-M7OB8QA>
2024-02-25 17:35:02 +0300

    dosyaların local ve pretty versiyonları yaratılarak debug'a uygun hale getirildi pose estimator uygulamasının scripti tekrar düzenlenerek takibe daha uygun hale getirildi pose estimator içine ikinci modelin yüklenmesi denendi ancak yapılamadı

--- mvp-pose-estimator/www/script.js
+++ mvp-pose-estimator/www/script.js
@@ -1,205 +1,149 @@
-console.log("script.js çalıştı");
 
-const model = bodySegmentation.SupportedModels.MediaPipeSelfieSegmentation; // or 'BodyPix'
-
-console.log("script.js 1");
-
-// console.log("model full");
-// console.log(model);
-
-// console.log("script.js 1.1");
-
-// console.log("model özet");
-// console.log(model.summary());
-
-const segmenterConfig = {
-  runtime: 'tfjs', //'mediapipe', // or 'tfjs'
-  modelType: 'general' // or 'landscape'
-};
-
-console.log("script.js 2");
-
-
-let segmenter = await bodySegmentation.createSegmenter(model, segmenterConfig);
-
-console.log("script.js 3");
-
-console.log("segmenter");
-console.log(segmenter);
-
-
-
-
-// const video = document.getElementById("webcam");
-// const canvasElement = document.getElementById("output_canvas"); 
-// let enableWebcamButton = document.getElementById("webcamButton");
-// let webcamRunning = false;
-
-// // Check if webcam access is supported.
-// const hasGetUserMedia = () => !!navigator.mediaDevices?.getUserMedia;
-
-// // If webcam supported, add event listener to button for when user
-// // wants to activate it.
-// if (hasGetUserMedia()) {
-//   enableWebcamButton = document.getElementById("webcamButton");
-//   enableWebcamButton.addEventListener("click", enableCam);
-// } else {
-//   console.warn("getUserMedia() is not supported by your browser");
-// }
-
-// // Enable the live webcam view and start detection.
-//  async function enableCam(event) {
-
-//   if (webcamRunning === true) {
-//     webcamRunning = false;
-//     enableWebcamButton.innerText = "ENABLE PREDICTIONS";
-//   } else {
-//     webcamRunning = true;
-//     enableWebcamButton.innerText = "DISABLE PREDICTIONS";
-//   }
-
-//   // getUsermedia parameters.
-//   const constraints = {
-//     video: true
-//   };
-// console.log('aa');
-//   // Activate the webcam stream.
-
-//   console.log(navigator.mediaDevices.getUserMedia);
-//   await navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
-//   console.log('bb');  
-//   video.srcObject = stream;
-//   console.log('cc');  
-//   video.addEventListener("loadeddata", predictWebcam);
-//   console.log('dd');  
-// });
-// console.log('ee');  
-// }
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
-import {
-    PoseLandmarker,
-    FilesetResolver,
-    DrawingUtils
-  } from "https://cdn.skypack.dev/@mediapipe/tasks-vision@0.10.0";
-
-  console.log('PoseLandmarker');
-  console.log(PoseLandmarker);
-  console.log('FilesetResolver');
-  console.log(FilesetResolver);
-  console.log('DrawingUtils');
-  console.log(DrawingUtils);
-  
-  const demosSection = document.getElementById("demos")
-  
-  let poseLandmarker = undefined
-  let runningMode = "IMAGE"
-  let enableWebcamButton
-  let webcamRunning = false
-  const videoHeight = "360px"
-  const videoWidth = "480px"
-  
-  // Before we can use PoseLandmarker class we must wait for it to finish
-  // loading. Machine Learning models can be large and take a moment to
-  // get everything needed to run.
-  const createPoseLandmarker = async () => {
-    const vision = await FilesetResolver.forVisionTasks(
-      "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0/wasm"
-    )
-    poseLandmarker = await PoseLandmarker.createFromOptions(vision, {
-      baseOptions: {
-        modelAssetPath: `https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_lite/float16/1/pose_landmarker_lite.task`,
-        delegate: "GPU"
-      },
-      runningMode: runningMode,
-      numPoses: 2
-    })
-    demosSection.classList.remove("invisible")
-  }
-  createPoseLandmarker()
-  
-  /********************************************************************
-  // Demo 2: Continuously grab image from webcam stream and detect it.
-  ********************************************************************/
-  
-  const video = document.getElementById("webcam")
-  const canvasElement = document.getElementById("output_canvas")
-  const canvasCtx = canvasElement.getContext("2d")
-  const drawingUtils = new DrawingUtils(canvasCtx)
-  
-  // Check if webcam access is supported.
-  const hasGetUserMedia = () => !!navigator.mediaDevices?.getUserMedia
-  
-  // If webcam supported, add event listener to button for when user
-  // wants to activate it.
-  if (hasGetUserMedia()) {
-    enableWebcamButton = document.getElementById("webcamButton")
-    enableWebcamButton.addEventListener("click", enableCam)
-  } else {
-    console.warn("getUserMedia() is not supported by your browser")
-  }
-  
-  // Enable the live webcam view and start detection.
-  function enableCam(event) {
-    if (!poseLandmarker) {
-      console.log("Wait! poseLandmaker not loaded yet.")
-      return
-    }
-  
-    if (webcamRunning === true) {
-      webcamRunning = false
-      enableWebcamButton.innerText = "ENABLE PREDICTIONS"
+async function setupCamera() {
+    const videoElement = document.getElementById('videoElement');
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        const stream = await navigator.mediaDevices.getUserMedia({ video: {} });
+        videoElement.srcObject = stream;
+        return new Promise(resolve => {
+            videoElement.onloadedmetadata = () => {
+                resolve(videoElement);
+            };
+        });
     } else {
-      webcamRunning = true
-      enableWebcamButton.innerText = "DISABLE PREDICTIONS"
+        throw new Error('Camera not available');
     }
-  
-    // getUsermedia parameters.
-    const constraints = {
-      video: true
-    }
-  
-    // Activate the webcam stream.
-    navigator.mediaDevices.getUserMedia(constraints).then(stream => {
-      video.srcObject = stream;
-      video.addEventListener("loadeddata", predictWebcam);
-    })
-  }
-  
-  let lastVideoTime = -1
-  async function predictWebcam() {
-    console.log("predictWebcam called");
-    canvasElement.style.height = videoHeight
-    video.style.height = videoHeight
-    canvasElement.style.width = videoWidth
-    video.style.width = videoWidth
-    // Now let's start detecting the stream.
-    if (runningMode === "IMAGE") {
-      runningMode = "VIDEO"
-      await poseLandmarker.setOptions({ runningMode: "VIDEO" })
-    }
-    let startTimeMs = performance.now()
-    if (lastVideoTime !== video.currentTime) {
-      lastVideoTime = video.currentTime
-      poseLandmarker.detectForVideo(video, startTimeMs, result => {
-        canvasCtx.save()
-        canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height)
-        for (const landmark of result.landmarks) {
-          drawingUtils.drawLandmarks(landmark, {
-            radius: data => DrawingUtils.lerp(data.from.z, -0.15, 0.1, 5, 1)
-          })
-          drawingUtils.drawConnectors(landmark, PoseLandmarker.POSE_CONNECTIONS)
-        }
-        canvasCtx.restore()
-      })
-    }
-  
-    // Call this function again to keep predicting when the browser is ready.
-    if (webcamRunning === true) {
-      window.requestAnimationFrame(predictWebcam)
-    }
-  }
-  
+}
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
+async function initPoseDetector(detectorConfig) {
+    return await poseDetection.createDetector(poseDetection.SupportedModels.MoveNet, detectorConfig);
+}
+
+
+
+
+
+function clearCanvas(ctx, canvas) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function drawPose(pose, ctx, drawConfig) {
+
+
+    const keypoints = pose.keypoints;
+    ctx.fillStyle = 'red';
+    ctx.strokeStyle = 'red';
+
+    // Draw keypoints
+    for (let i = 0; i < keypoints.length; i++) {
+        const keypoint = keypoints[i];
+        if (keypoint.score > drawConfig.scoretreshold) {
+            ctx.beginPath();
+            ctx.arc(keypoint.x, keypoint.y, 3, 0, 2 * Math.PI);
+            ctx.fill();
+            if (drawConfig.labels) {
+                ctx.fillStyle = 'black';
+                ctx.font = 'bold 20px Arial';
+                ctx.textAlign = 'right';
+                ctx.textBaseline = 'top';
+                ctx.fillText(keypoint.name, keypoint.x, keypoint.y);
+            }
+        }
+    }
+
+    // Draw lines between keypoints
+    const adjacentKeyPoints = poseAdjacentKeyPoints(pose);
+    ctx.beginPath();
+
+    for (let i = 0; i < adjacentKeyPoints.length; i++) {
+
+        const [point1, point2] = adjacentKeyPoints[i];
+        if (point1.score > drawConfig.scoretreshold && point2.score > drawConfig.scoretreshold) {
+            ctx.moveTo(point1.x, point1.y);
+            ctx.lineTo(point2.x, point2.y);
+        }
+    }
+    ctx.stroke();
+}
+
+function poseAdjacentKeyPoints(pose) {
+    const adjacentKeyPoints = [
+        ['nose', 'left_eye'],
+        ['left_eye', 'right_eye'],
+        ['right_eye', 'nose'],
+        ['left_shoulder', 'right_shoulder'],
+        ['left_elbow', 'left_shoulder'],
+        ['right_elbow', 'right_shoulder'],
+        ['left_wrist', 'left_elbow'],
+        ['right_wrist', 'right_elbow'],
+        ['left_hip', 'right_hip'],
+        ['left_knee', 'left_hip'],
+        ['right_knee', 'right_hip'],
+        ['left_ankle', 'left_knee'],
+        ['right_ankle', 'right_knee']
+    ];
+
+    return adjacentKeyPoints.map(([point1, point2]) => {
+        const keypoint1 = pose.keypoints.find(kp => kp.name === point1);
+        const keypoint2 = pose.keypoints.find(kp => kp.name === point2);
+        // if (!keypoint1) console.error(`Keypoint '${point1}' not found in pose`);
+        // if (!keypoint2) console.error(`Keypoint '${point2}' not found in pose`);
+        return [keypoint1, keypoint2];
+    });
+}
+
+
+async function initHtmlObjects() {
+    const canvas = document.getElementById('outputCanvas');
+    const ctx = canvas.getContext('2d');
+    const videoElement = await setupCamera();
+    return {
+        canvas: canvas,
+        ctx: ctx,
+        videoElement: videoElement,
+    }
+}
+
+async function readDrawConfig() {
+    var scoretreshold = document.getElementById('score-treshold').value;
+    var labels = document.getElementById('labels').checked;
+    return {
+        scoretreshold: scoretreshold,
+        labels: labels,
+    }
+
+}
+
+
+async function main() {
+    // const htmlObjects = await initHtmlObjects();
+    // const detectorConfig = { modelType: poseDetection.movenet.modelType.SINGLEPOSE_THUNDER };
+    // const detector = await initPoseDetector(detectorConfig);
+
+    //ikinci model işleri
+    // await tf.enableProdMode();
+    // await tf.setBackend("webgl");
+    // await tf.ENV.set("DEBUG", false);
+    // await tf.ready(); 
+    // labelDetectorModel = await tf.loadGraphModel('./canavar'); //bizimki şimdilik layers model bu hata veriyor
+    //var labelDetectorModel = await tf.loadLayersModel('./canavar/model.json'); // hobaaa tfjs-node üzerinde var olan loadLayersModel tfjs-core da yok  
+    
+    
+
+    async function poseLooper() {
+        console.log('poseLooper çalıştı');
+
+        const poses = await detector.estimatePoses(htmlObjects.videoElement, { maxPoses: 1, flipHorizontal: false });
+        drawConfig = await readDrawConfig();
+        clearCanvas(htmlObjects.ctx, htmlObjects.canvas);
+        if (poses && poses[0] !== undefined) {
+            drawPose(poses[0], htmlObjects.ctx, drawConfig);
+        }
+
+        setTimeout(poseLooper, 1);
+    }
+
+    poseLooper();
+}
+
+main();
 
