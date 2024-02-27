cae5035934f82a05b77dabbd538339319d2403ba
barancankatesen <baran@DESKTOP-M7OB8QA>
2024-02-27 22:34:15 +0300

    iki modelin birlikte çalıştığı ilk versiyon

--- mvp-pose-estimator/www/script.js
+++ mvp-pose-estimator/www/script.js
@@ -113,30 +113,92 @@ async function readDrawConfig() {
 
 }
 
+function poseToPoseArray(pose) {
+    
+    poseArray = [];
+
+    for (let index = 0; index < pose[0].keypoints.length; index++) {
+        poseArray.push(pose[0].keypoints[index].x);
+        poseArray.push(pose[0].keypoints[index].y);
+    }
+
+    return poseArray;
+  }
+
+  function normalizeScalar(tensor, min, max) {
+    const result = tf.tidy(function () {
+      const MIN_VALUES = tf.scalar(min);
+      //console.log('normalizeScalar MIN_VALUES');
+      const MAX_VALUES = tf.scalar(max);
+      //console.log('normalizeScalar MAX_VALUES');
+  
+      // Now calculate subtract the MIN_VALUE from every value in the Tensor
+      // And store the results in a new Tensor.
+      const TENSOR_SUBTRACT_MIN_VALUE = tf.sub(tensor, MIN_VALUES);
+      //console.log('normalizeScalar TENSOR_SUBTRACT_MIN_VALUE');
+  
+      // Calculate the range size of possible values.
+      const RANGE_SIZE = tf.sub(MAX_VALUES, MIN_VALUES);
+      //console.log('normalizeScalar RANGE_SIZE');
+  
+      // Calculate the adjusted values divided by the range size as a new Tensor.
+      const NORMALIZED_VALUES = tf.div(TENSOR_SUBTRACT_MIN_VALUE, RANGE_SIZE);
+      //console.log('normalizeScalar NORMALIZED_VALUES');
+  
+      // Return the important tensors.
+      return NORMALIZED_VALUES;
+    });
+    return result;
+  }  
 
 async function main() {
-    // const htmlObjects = await initHtmlObjects();
-    // const detectorConfig = { modelType: poseDetection.movenet.modelType.SINGLEPOSE_THUNDER };
-    // const detector = await initPoseDetector(detectorConfig);
+    const htmlObjects = await initHtmlObjects();
+    const detectorConfig = { modelType: poseDetection.movenet.modelType.SINGLEPOSE_THUNDER };
+    const detector = await initPoseDetector(detectorConfig);
 
     //ikinci model işleri
-    // await tf.enableProdMode();
-    // await tf.setBackend("webgl");
-    // await tf.ENV.set("DEBUG", false);
-    // await tf.ready(); 
+    await tf.enableProdMode();
+    await tf.setBackend("webgl");
+    await tf.ENV.set("DEBUG", false);
+    await tf.ready(); 
     // labelDetectorModel = await tf.loadGraphModel('./canavar'); //bizimki şimdilik layers model bu hata veriyor
-    //var labelDetectorModel = await tf.loadLayersModel('./canavar/model.json'); // hobaaa tfjs-node üzerinde var olan loadLayersModel tfjs-core da yok  
+    var labelDetectorModel = await tf.loadLayersModel('./canavar/model.json'); // hobaaa tfjs-node üzerinde var olan loadLayersModel tfjs-core da yok  
     
     
 
     async function poseLooper() {
-        console.log('poseLooper çalıştı');
 
         const poses = await detector.estimatePoses(htmlObjects.videoElement, { maxPoses: 1, flipHorizontal: false });
+        console.log('poses');
+        console.log(poses);
+        console.log('/poses');
+
+
         drawConfig = await readDrawConfig();
         clearCanvas(htmlObjects.ctx, htmlObjects.canvas);
         if (poses && poses[0] !== undefined) {
             drawPose(poses[0], htmlObjects.ctx, drawConfig);
+            poseArray = poseToPoseArray(poses);
+  
+            let modelInput = normalizeScalar(tf.tensor1d(poseArray), 0, 255);
+            let expandedTensor = tf.expandDims(modelInput);
+             
+            let modelResult = labelDetectorModel.predict(expandedTensor);
+
+            modelResult.print();
+
+            // var rank2Array = JSON.parse(modelResult.print());
+
+            // var maxIndex = rank2Array.indexOf(Math.max(...rank2Array));
+
+            // document.getElementById('outputindex').value = maxIndex;
+
+            //modelresult.print ile üretilen rank2 arrayin 
+            //ilk elemanının maximum değerdeki öğesinin 
+            //index numarasını bulalım
+            //ekrana textboxa yazalım
+
+
         }
 
         setTimeout(poseLooper, 1);
